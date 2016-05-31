3ddb67e4267e8f609798a7c09f4c2627c8366beb
Sergio de la Garza <sergio@delagarza.io>
2016-05-30 19:31:08 -0500

    Added listening info

--- generators/app/templates/skeleton/bin/server.js
+++ generators/app/templates/skeleton/bin/server.js
@@ -5,3 +5,5 @@ var path = require('path');
 var neonode = require(path.join(process.cwd(), '/lib/core'));
 
 neonode._serverStart();
+
+logger.info('Server started listening on http://localhost:' + CONFIG[CONFIG.environment].port);
