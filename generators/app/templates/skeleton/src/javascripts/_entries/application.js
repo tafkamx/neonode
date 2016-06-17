177229568230bebbcc07e571dd049b9fad682988
Noel Delgado <auhcsei@gmail.com>
2016-06-16 20:43:59 -0500

    update skeleton files

--- generators/app/templates/skeleton/src/javascripts/_entries/application.js
+++ generators/app/templates/skeleton/src/javascripts/_entries/application.js
@@ -1,1 +1,5 @@
-console.log('application');
+import foo from '../modules/foo';
+import { bar, baz } from '../modules/bar';
+
+console.log(`application::module:${foo}`);
+console.log(`application::module:${bar}:${baz()}`);
